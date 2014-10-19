ec43e670fd6674d2b0300dec1fe739fb9ea644c1
Yordan Darakchiev <iordan93@gmail.com>
2014-10-19 15:54:20 +0300

    .

--- Team-Eranikus-Game-Project/WorldOfASCIITanks/Program.cs
+++ Team-Eranikus-Game-Project/WorldOfASCIITanks/Program.cs
@@ -1,9 +1,9 @@
 ﻿using System;
+
 using WorldOfASCIITanks.GameEngine;
 using WorldOfASCIITanks.GameObjects.World;
 using WorldOfASCIITanks.GameObjects.World.Map;
 using WorldOfASCIITanks.GameObjects.World.Movable;
-using WorldOfASCIITanks.Interfaces;
 using WorldOfASCIITanks.Rendering;
 
 namespace WorldOfASCIITanks
@@ -15,40 +15,26 @@ namespace WorldOfASCIITanks
         private static void Main()
         {
             ConsoleSettings.PrepareConsole();
-            var gameObjects = MapParser.ParseMap("../../WorldMaps/map.txt");
 
-
-            //char[,] heroBody = new char[1, 1] { { '*' } };
-            //MainCharacter hero = new MainCharacter(new MatrixCoords(1, 1), heroBody);
-			KeyboardInterface keyboard = new KeyboardInterface();
-            //NPC enemy = new NPC(new MatrixCoords(3, 3), new char[,] { { '@' } });
+            KeyboardInterface keyboard = new KeyboardInterface();
+            NPC enemy = new NPC(new MatrixCoords(3, 3), new char[,] { { '@' } });
             ConsoleRenderer renderer = new ConsoleRenderer(ConsoleSettings.ConsoleHeight, ConsoleSettings.ConsoleWidth);
             GameEngine.GameEngine gameEngine = new GameEngine.GameEngine(renderer, keyboard);
-            foreach (var item in gameObjects)
-            {
-                renderer.EnqueueForRendering(item);
-            }
-            renderer.RenderAll();
-            //gameEngine.AddObject(hero);
-            //gameEngine.AddObject(enemy);
 
-            //keyboard.OnDownPressed += (sender, eventInfo) => { hero.Move(Direction.Down); };
-            //keyboard.OnLeftPressed += (sender, eventInfo) => { hero.Move(Direction.Left); };
-            //keyboard.OnRightPressed += (sender, eventInfo) => { hero.Move(Direction.Right); };
-            //keyboard.OnUpPressed += (sender, eventInfo) => { hero.Move(Direction.Top); };
             Console.WriteLine("Please select your Hero: \nPress 1 for  Mage\nPress 2 for  Thief\nPress 3 for  Warrior");
             int choiseHero = int.Parse(Console.ReadLine());
-                // TODO implement interface for the choice of type of character
+            // TODO implement interface for the choice of type of character
             MainCharacter hero = HeroChoice(choiseHero);
 
-            //gameEngine.AddObject(hero);
-            //gameEngine.AddObject(enemy);
+            gameEngine.AddObject(hero);
+            gameEngine.AddObject(enemy);
 
             keyboard.OnDownPressed += (sender, eventInfo) => { hero.Move(Direction.Down); };
             keyboard.OnLeftPressed += (sender, eventInfo) => { hero.Move(Direction.Left); };
             keyboard.OnRightPressed += (sender, eventInfo) => { hero.Move(Direction.Right); };
             keyboard.OnUpPressed += (sender, eventInfo) => { hero.Move(Direction.Top); };
-            //gameEngine.Run();
+
+            gameEngine.Run();
         }
 
         public static MainCharacter HeroChoice(int heroChoice)
