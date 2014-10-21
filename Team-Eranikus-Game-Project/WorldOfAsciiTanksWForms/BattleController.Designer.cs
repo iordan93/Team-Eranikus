c8a341fe2d311c415997e9ee1a1503f4b0f8021c
momchilDishev <mo.dishev@gmail.com>
2014-10-22 01:07:00 +0300

    windows forms battle interface

---
+++ Team-Eranikus-Game-Project/WorldOfAsciiTanksWForms/BattleController.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace WorldOfAsciiTanksWForms
+{
+    partial class BattleController
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BattleController));
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblLevel = new System.Windows.Forms.Label();
+            this.lblHitPoints = new System.Windows.Forms.Label();
+            this.lblManaPoints = new System.Windows.Forms.Label();
+            this.lblExperience = new System.Windows.Forms.Label();
+            this.lblHitPointsOpponent = new System.Windows.Forms.Label();
+            this.lblLevelOpponent = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.OpponentName = new System.Windows.Forms.Label();
+            this.cboPotions = new System.Windows.Forms.ComboBox();
+            this.cboWeapon = new System.Windows.Forms.ComboBox();
+            this.btnUseWeapon = new System.Windows.Forms.Button();
+            this.btnUse = new System.Windows.Forms.Button();
+            this.PlayerPicture = new System.Windows.Forms.PictureBox();
+            this.OpponentPicture = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PlayerPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OpponentPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(91, 46);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(0, 13);
+            this.lblName.TabIndex = 1;
+            this.lblName.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Hit Points";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Mana Points";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 148);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Experience";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 74);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Level";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // lblLevel
+            // 
+            this.lblLevel.AutoSize = true;
+            this.lblLevel.Location = new System.Drawing.Point(91, 74);
+            this.lblLevel.Name = "lblLevel";
+            this.lblLevel.Size = new System.Drawing.Size(0, 13);
+            this.lblLevel.TabIndex = 6;
+            // 
+            // lblHitPoints
+            // 
+            this.lblHitPoints.AutoSize = true;
+            this.lblHitPoints.Location = new System.Drawing.Point(91, 96);
+            this.lblHitPoints.Name = "lblHitPoints";
+            this.lblHitPoints.Size = new System.Drawing.Size(0, 13);
+            this.lblHitPoints.TabIndex = 7;
+            // 
+            // lblManaPoints
+            // 
+            this.lblManaPoints.AutoSize = true;
+            this.lblManaPoints.Location = new System.Drawing.Point(91, 119);
+            this.lblManaPoints.Name = "lblManaPoints";
+            this.lblManaPoints.Size = new System.Drawing.Size(0, 13);
+            this.lblManaPoints.TabIndex = 8;
+            // 
+            // lblExperience
+            // 
+            this.lblExperience.AutoSize = true;
+            this.lblExperience.Location = new System.Drawing.Point(91, 148);
+            this.lblExperience.Name = "lblExperience";
+            this.lblExperience.Size = new System.Drawing.Size(0, 13);
+            this.lblExperience.TabIndex = 9;
+            // 
+            // lblHitPointsOpponent
+            // 
+            this.lblHitPointsOpponent.AutoSize = true;
+            this.lblHitPointsOpponent.Location = new System.Drawing.Point(439, 96);
+            this.lblHitPointsOpponent.Name = "lblHitPointsOpponent";
+            this.lblHitPointsOpponent.Size = new System.Drawing.Size(0, 13);
+            this.lblHitPointsOpponent.TabIndex = 17;
+            this.lblHitPointsOpponent.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // lblLevelOpponent
+            // 
+            this.lblLevelOpponent.AutoSize = true;
+            this.lblLevelOpponent.Location = new System.Drawing.Point(439, 74);
+            this.lblLevelOpponent.Name = "lblLevelOpponent";
+            this.lblLevelOpponent.Size = new System.Drawing.Size(0, 13);
+            this.lblLevelOpponent.TabIndex = 16;
+            this.lblLevelOpponent.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(360, 74);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(33, 13);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "Level";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(360, 96);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(52, 13);
+            this.label13.TabIndex = 12;
+            this.label13.Text = "Hit Points";
+            this.label13.Click += new System.EventHandler(this.label13_Click);
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(439, 46);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(0, 13);
+            this.label14.TabIndex = 11;
+            this.label14.Click += new System.EventHandler(this.label14_Click);
+            // 
+            // OpponentName
+            // 
+            this.OpponentName.AutoSize = true;
+            this.OpponentName.Location = new System.Drawing.Point(360, 46);
+            this.OpponentName.Name = "OpponentName";
+            this.OpponentName.Size = new System.Drawing.Size(54, 13);
+            this.OpponentName.TabIndex = 10;
+            this.OpponentName.Text = "Opponent";
+            this.OpponentName.Click += new System.EventHandler(this.label15_Click);
+            // 
+            // cboPotions
+            // 
+            this.cboPotions.FormattingEnabled = true;
+            this.cboPotions.Location = new System.Drawing.Point(15, 233);
+            this.cboPotions.Name = "cboPotions";
+            this.cboPotions.Size = new System.Drawing.Size(121, 21);
+            this.cboPotions.TabIndex = 18;
+            this.cboPotions.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // cboWeapon
+            // 
+            this.cboWeapon.FormattingEnabled = true;
+            this.cboWeapon.Location = new System.Drawing.Point(15, 204);
+            this.cboWeapon.Name = "cboWeapon";
+            this.cboWeapon.Size = new System.Drawing.Size(121, 21);
+            this.cboWeapon.TabIndex = 19;
+            // 
+            // btnUseWeapon
+            // 
+            this.btnUseWeapon.Location = new System.Drawing.Point(142, 202);
+            this.btnUseWeapon.Name = "btnUseWeapon";
+            this.btnUseWeapon.Size = new System.Drawing.Size(75, 23);
+            this.btnUseWeapon.TabIndex = 20;
+            this.btnUseWeapon.Text = "Attack";
+            this.btnUseWeapon.UseVisualStyleBackColor = true;
+            // 
+            // btnUse
+            // 
+            this.btnUse.Location = new System.Drawing.Point(143, 231);
+            this.btnUse.Name = "btnUse";
+            this.btnUse.Size = new System.Drawing.Size(75, 23);
+            this.btnUse.TabIndex = 21;
+            this.btnUse.Text = "Use";
+            this.btnUse.UseVisualStyleBackColor = true;
+            this.btnUse.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // PlayerPicture
+            // 
+            this.PlayerPicture.Image = ((System.Drawing.Image)(resources.GetObject("PlayerPicture.Image")));
+            this.PlayerPicture.Location = new System.Drawing.Point(15, 301);
+            this.PlayerPicture.Name = "PlayerPicture";
+            this.PlayerPicture.Size = new System.Drawing.Size(222, 189);
+            this.PlayerPicture.TabIndex = 22;
+            this.PlayerPicture.TabStop = false;
+            this.PlayerPicture.Click += new System.EventHandler(this.PlayerPicture_Click);
+            // 
+            // OpponentPicture
+            // 
+            this.OpponentPicture.Image = ((System.Drawing.Image)(resources.GetObject("OpponentPicture.Image")));
+            this.OpponentPicture.Location = new System.Drawing.Point(314, 301);
+            this.OpponentPicture.Name = "OpponentPicture";
+            this.OpponentPicture.Size = new System.Drawing.Size(222, 189);
+            this.OpponentPicture.TabIndex = 23;
+            this.OpponentPicture.TabStop = false;
+            this.OpponentPicture.Click += new System.EventHandler(this.OpponentPicture_Click);
+            // 
+            // BattleController
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.OpponentPicture);
+            this.Controls.Add(this.PlayerPicture);
+            this.Controls.Add(this.btnUse);
+            this.Controls.Add(this.btnUseWeapon);
+            this.Controls.Add(this.cboWeapon);
+            this.Controls.Add(this.cboPotions);
+            this.Controls.Add(this.lblHitPointsOpponent);
+            this.Controls.Add(this.lblLevelOpponent);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.OpponentName);
+            this.Controls.Add(this.lblExperience);
+            this.Controls.Add(this.lblManaPoints);
+            this.Controls.Add(this.lblHitPoints);
+            this.Controls.Add(this.lblLevel);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.label1);
+            this.Name = "BattleController";
+            this.Text = "BattleControler";
+            this.Load += new System.EventHandler(this.BattleController_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PlayerPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OpponentPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblLevel;
+        private System.Windows.Forms.Label lblHitPoints;
+        private System.Windows.Forms.Label lblManaPoints;
+        private System.Windows.Forms.Label lblExperience;
+        private System.Windows.Forms.Label lblHitPointsOpponent;
+        private System.Windows.Forms.Label lblLevelOpponent;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label OpponentName;
+        private System.Windows.Forms.ComboBox cboPotions;
+        private System.Windows.Forms.ComboBox cboWeapon;
+        private System.Windows.Forms.Button btnUseWeapon;
+        private System.Windows.Forms.Button btnUse;
+        private System.Windows.Forms.PictureBox PlayerPicture;
+        private System.Windows.Forms.PictureBox OpponentPicture;
+    }
+}
---
+++ Team-Eranikus-Game-Project/WorldOfAsciiTanksWForms/BattleController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorldOfAsciiTanksWForms
+{
+    public partial class BattleController : Form
+    {
+        public BattleController()
+        {
+            InitializeComponent();
+        }
+
+        private void BattleController_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PlayerPicture_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OpponentPicture_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
